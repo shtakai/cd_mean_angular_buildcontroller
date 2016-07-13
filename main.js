f908663fd20d214930d23d1fde397cd9fc9cfa0a
Alyson Takai <shtakai@gmail.com>
2016-07-13 16:48:13 -0700

    ng-repeat

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ let myApp = angular.module('myApp',[]);
 
 myApp.controller('foodsController', function(){
   console.log('load');
+  this.favoriteFoods = [
+    'Pizza',
+    'BBQ',
+  ];
+
+  this.addFood = function(){
+    let _food = this.favoriteFood;
+    console.log(`new food: ${_food}`);
+  }
 
 });
 
