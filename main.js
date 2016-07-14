84ae9a06590557e06a3809d77ec79e438048308c
Alyson Takai <shtakai@gmail.com>
2016-07-13 19:39:25 -0700

    implemented ng-change

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ let myApp = angular.module('myApp',[]);
 
 myApp.controller('foodsController', function(){
   console.log('load');
-  this.favoriteFoods = [
+  let _favoriteFoods = [
     'Pizza',
     'BBQ',
   ];
-
+  this.favoriteFoods = _favoriteFoods;
   this.addFood = function(){
     let _food = this.favoriteFood;
     console.log(`new food: ${_food}`);
+    _food = _food || '';
+    if(_food.trim() === ''){
+      return;
+    }
+    this.favoriteFoods.unshift(_food);
+    this.favoriteFood = '';
+  }
+  this.find = function(){
+    this.findText = this.findText || '';
+     console.log(`find clicked: ${this.findText}`);
+
   }
 
 });
